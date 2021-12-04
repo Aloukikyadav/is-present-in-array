4c7e0faecaf638fb66ab3c6e5c313b400315bd93
Aloukikyadav <65151504+Aloukikyadav@users.noreply.github.com>
2021-12-05 00:02:07 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+var arr = [ 3, 7, 0, 9, 8]
+var size = 5
+for(var i = 0 ; i<size ; i++) {
+       var k = 0 ;
+       var available = "" ;
+        k = arr[i] ;
+        if(k===42){
+            available = "Yes"  ; 
+            
+        }
+        else {
+            available = "No" ;   
+             }
+        }
+
+        console.log(available);
